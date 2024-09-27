635a5215c30101552bea46aadfe8d7f15ba3e15e
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-09-27 18:39:02 -0300

    Atualiza tamanho da senha gerada e estiliza input
    Aumenta a flexibilidade do gerador de senhas permitindo que o usuário escolha o tamanho da senha a ser gerada. Além disso, estiliza o input para melhorar a aparência e usabilidade.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { useState } from "react"
 function App() {
 const [password, setPassword] = useState("")
 const [copyText, setCopyText] = useState("Copiar")
+const [passwordSize, setPasswordSize] = useState(12)
 
 function generate() {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>/?";
-  const length = 12;
+  // const length = 12;
   let newPassword = "";
-  for (let i=0; i<length; i++) {
+  for (let i=0; i<passwordSize; i++) {
     const position = Math.floor(Math.random() * characters.length)
 newPassword += characters[position]
   }
@@ -33,7 +34,17 @@ function copyToClipboard() {
   return (
     <>
     <h1>Gerador de senhas</h1>
-    <button onClick={generate}>{}Gerar!</button>
+    <div>  
+    <label htmlFor="passwordSize">Tamanho: </label>
+    <input 
+    type="number" 
+    id ="passwordSize" 
+    min={1}
+    value={passwordSize}
+    onChange={(ev) => setPasswordSize(event.target.value)}
+    />
+    </div>
+    <button onClick={generate}>Gerar senha de {passwordSize} caracteres!</button>
     <button onClick={copyToClipboard}>{copyText}</button>
     <div>{password}Senha</div>
     </>
